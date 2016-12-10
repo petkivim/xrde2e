2f2708b8ca7f47d8cb7ef7326432da818756231d
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-12-10 16:24:25 +0200

    UI updated.

--- ui/src/js/xrde2e.js
+++ ui/src/js/xrde2e.js
@@ -6,15 +6,17 @@ jQuery(document).ready(function ($) {
             success: function (data) {
                 //console.log(data); 
                 var html = '<table><tr>';
-                html += '<th>Server</th><th>Duration</th><th>Received</th><th>Request Id</th><th>Status</th><th>Info</th>';
+                html += '<th>Server</th><th>Duration</th><th>Created</th><th>Request Id</th><th>Status</th><th>Info</th>';
                 html += '</tr>';
                 jQuery.each(data, function (i, val) {
-                    html += '<tr><td class="col1">' + val.securityServer + '</td>';
-                    html += '<td class="col2">' + val.duration + ' ms</td>';
-                    html += '<td class="col3">' + (val.end === null ? '-' : val.end) + '</td>';
+                    var tooltipCol1 = '<span data-tooltip="' + val.securityServer + '" data-tooltip-position="right">' + (val.label.length > 0 ? val.label : val.securityServer) + '</span>';
+                    var tooltipCol2 = '<span data-tooltip="Sent: ' + val.begin + ' Received:' + val.end + '" data-tooltip-position="left">' + val.duration + '</span>';
+                    html += '<tr><td class="col1">' + tooltipCol1 + '</td>';
+                    html += '<td class="col2">' + tooltipCol2 + ' ms</td>';
+                    html += '<td class="col3">' + val.createdDate + '</td>';
                     html += '<td class="col4">' + val.requestId + '</td>';
                     html += '<td class="col5"><span class="label ' + (val.status ? 'success' : 'failure') + '">' + (val.status ? 'OK' : 'NOK') + '</span></td>';
-                    html += '<td class="col6">' + val.faultCode + '</td></tr>';
+                    html += '<td class="col6">' + val.faultCode + '<br /> ' + val.faultString + '</td></tr>';
                 });
                 html += '</tr></table>';
                 $("div#container").html(html);
@@ -28,7 +30,7 @@ jQuery(document).ready(function ($) {
             },
             complete: function () {
                 // Schedule the next request when the current one's complete
-                setTimeout(worker, 30000);
+                setTimeout(worker, 5000);
             }
         });
     })();
