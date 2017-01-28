2205214e2fa9ef3b37f3b229681fa92a10ef8a50
Petteri Kivimäki <dinky_jackson@hotmail.com>
2017-01-28 17:45:06 +0200

    UI implementation rewritten.
    UI implementation rewritten using AngularJS. Search added.

--- ui/src/js/xrde2e.js
+++ ui/src/js/xrde2e.js
@@ -1,79 +1,60 @@
-jQuery(document).ready(function ($) {
-    $("#dialog").dialog({
-        autoOpen: false,
-        width: 1600,
-        maxHeight: 600
-    });
+var app = angular.module('xrde2e', ['ui.bootstrap']);
 
-    (function worker() {
-        $.ajax({
-            url: '/api/v1/current',
-            dataType: 'json',
-            success: function (data) {
-                //console.log(data); 
-                var html = '<table><tr>';
-                html += '<th>Server</th><th>Duration</th><th>Created</th><th>Request Id</th><th>Status</th><th>Info</th>';
-                html += '</tr>';
-                jQuery.each(data, function (i, val) {
-                    var tooltipCol1 = '<span data-tooltip="' + val.securityServer + '" data-tooltip-position="right">' + (val.label.length > 0 ? val.label : val.securityServer) + '</span>';
-                    var tooltipCol2 = '<span data-tooltip="Sent: ' + val.begin + ' Received:' + val.end + '" data-tooltip-position="left">' + val.duration + '</span>';
-                    html += '<tr><td class="col1">' + tooltipCol1 + '</td>';
-                    html += '<td class="col2">' + tooltipCol2 + ' ms</td>';
-                    html += '<td class="col3">' + val.createdDate + '</td>';
-                    html += '<td class="col4">' + val.requestId + '</td>';
-                    html += '<td class="col5"><span class="label ' + (val.status ? 'success' : 'failure') + '">' + (val.status ? 'OK' : 'NOK') + '</span></td>';
-                    html += '<td class="col6">' + val.faultCode + '<br /> ' + val.faultString + '</td></tr>';
-                });
-                html += '</tr></table>';
-                $("div#container").html(html);
-                var now = new Date();
-                $("div#updated").html("Last update: " + now.toLocaleDateString("fi-FI") + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
-                $(".col1").click(function () {
-                    var server = $(this).find('span').attr("data-tooltip");
-                    $("div#dialog").dialog("option", "title", server);
-                    update_dialog(server);
-                    $("#dialog").dialog("open");
-                });
-            },
-            error: function (data, status, error) {
-                console.log(data);
-                console.log(status);
-                console.log(error);
-            },
-            complete: function () {
-                // Schedule the next request when the current one's complete
-                setTimeout(worker, 30000);
-            }
+app.controller('mainController', function ($scope, $uibModal, $http, $timeout) {
+    $scope.search = '';
+
+    $scope.showModal = function (serverIdentifier) {
+        $scope.opts = {
+            backdrop: true,
+            backdropClick: true,
+            dialogFade: true,
+            keyboard: true,
+            templateUrl: 'templates/table_detailed.html',
+            controller: ModalInstanceCtrl,
+            resolve: {} // empty storage
+        };
+
+        $scope.opts.resolve.item = function () {
+            return angular.copy({serverIdentifier: serverIdentifier});
+        }
+
+        var modalInstance = $uibModal.open($scope.opts);
+
+        modalInstance.result.then(function () {
+            //on ok button press 
+        }, function () {
+            //on cancel button press
         });
-    })();
-});
+    };
 
-function update_dialog(server) {
-    $("div#dialog").html('');
-    $.ajax({
-        url: '/api/v1/history/' + server,
-        dataType: 'json',
-        success: function (data) {
-            var html = '<table><tr>';
-            html += '<th>Duration</th><th>Created</th><th>Request Id</th><th>Status</th><th>Info</th>';
-            html += '</tr>';
-            jQuery.each(data, function (i, val) {
-                var tooltipCol2 = '<span data-tooltip="Sent: ' + val.begin + ' Received:' + val.end + '" data-tooltip-position="right">' + val.duration + '</span>';
-                html += '<td class="col2">' + tooltipCol2 + ' ms</td>';
-                html += '<td class="col3">' + val.createdDate + '</td>';
-                html += '<td class="col4">' + val.requestId + '</td>';
-                html += '<td class="col5"><span class="label ' + (val.status ? 'success' : 'failure') + '">' + (val.status ? 'OK' : 'NOK') + '</span></td>';
-                html += '<td class="col6">' + val.faultCode + '<br /> ' + val.faultString + '</td></tr>';
-            });
-            html += '</tr></table>';
-            $("div#dialog").html(html);
-        },
-        error: function (data, status, error) {
+    var update = function () {
+        $http.get('/api/v1/current').then(function (data) {
+            $scope.targets = data.data;
+            $scope.timestamp = new Date();
+        }), function (data, status, headers) {
             console.log(data);
             console.log(status);
-            console.log(error);
-        },
-        complete: function () {
-        }
-    });
-}
+            console.log(headers);
+        };
+        $timeout(update, 30000);
+    };
+    $timeout(update, 0);
+});
+
+var ModalInstanceCtrl = function ($scope, $http, $uibModalInstance, item) {
+    $http.get('/api/v1/history/' + item.serverIdentifier).then(function (data) {
+        $scope.targets = data.data;
+        $scope.serverIdentifier = item.serverIdentifier;
+    }), function (data, status, headers) {
+        console.log(data);
+        console.log(status);
+        console.log(headers);
+    };
+    $scope.ok = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
